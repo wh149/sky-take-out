42738e3b5f7430ed45a5a69182a2ab275d423488
Hao Wang <1493390671@qq.com>
2025-05-20 17:38:21 +1000

    新增员工功能

--- backend/sky-take-out/sky-common/src/main/java/com/sky/context/BaseContext.java
+++ backend/sky-take-out/sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -1,7 +1,10 @@
 package com.sky.context;
 
 public class BaseContext {
-
+    // threadLocal
+    // 内存泄漏 ？
+    // 看你们都提出了会产生内存泄露问题，但是没人说明解决方法，这里帖个解决方法,重写拦截器的afterCompletion方法，并添加BaseContext.removeCurrentId()
+    
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
     public static void setCurrentId(Long id) {
--- backend/sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ backend/sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -66,6 +67,7 @@ public class EmployeeServiceImpl implements EmployeeService {
      * @param employeeDTO
      */
     public void save(EmployeeDTO employeeDTO) {
+        System.out.println("当前线程的id:" + Thread.currentThread().getId());
         Employee employee = new Employee();
         
         //对象属性拷贝
@@ -83,9 +85,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateTime(LocalDateTime.now());
 
         //设置创建人id和修改人id
-        //TODO 后期需要改成当前登陆用户的id
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
         employeeMapper.insert(employee);
     }
 
